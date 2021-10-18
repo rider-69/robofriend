d5dc7a5abdfc83be7fc60c34378ba6e5c0f6941d
kunal verma <72096476+rider-69@users.noreply.github.com>
2021-10-18 16:57:15 +0530

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App   from './App' ;
+import "tachyons";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+<App/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
+reportWebVitals();
