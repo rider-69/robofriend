15b5fe4f0ca1625645eae5710926a0589898dac9
kunal verma <72096476+rider-69@users.noreply.github.com>
2021-10-18 18:04:26 +0530

    made

--- src/robots.js
+++ src/robots.js
@@ -1,61 +1,61 @@
 export const robots = [  
     {
       id: 1,
-      name: 'Leanne Graham',
+      name: 'Kunal verma ',
       username: 'Bret',
       email: 'Sincere@april.biz'
     },
     {
       id: 2,
-      name: 'Ervin Howell',
+      name: 'Rishab Anand',
       username: 'Antonette',
       email: 'Shanna@melissa.tv'
     },
     {
       id: 3,
-      name: 'Clementine Bauch',
+      name: 'Angel lakshya',
       username: 'Samantha',
       email: 'Nathan@yesenia.net'
     },
     {
       id: 4,
-      name: 'Patricia Lebsack',
+      name: 'Priyanshi',
       username: 'Karianne',
       email: 'Julianne.OConner@kory.org'
     },
     {
       id: 5,
-      name: 'Chelsey Dietrich',
+      name: 'Vinay Sangwan',
       username: 'Kamren',
       email: 'Lucio_Hettinger@annie.ca'
     },
     {
       id: 6,
-      name: 'Mrs. Dennis Schulist',
+      name: 'Mrs. Anant chor',
       username: 'Leopoldo_Corkery',
       email: 'Karley_Dach@jasper.info'
     },
     {
       id: 7,
-      name: 'Kurtis Weissnat',
+      name: 'Yash trehan',
       username: 'Elwyn.Skiles',
       email: 'Telly.Hoeger@billy.biz'
     },
     {
       id: 8,
-      name: 'Nicholas Runolfsdottir V',
+      name: ' Simran thakur',
       username: 'Maxime_Nienow',
       email: 'Sherwood@rosamond.me'
     },
     {
       id: 9,
-      name: 'Glenna Reichert',
+      name: 'Vanshika verma',
       username: 'Delphine',
       email: 'Chaim_McDermott@dana.io'
     },
     {
       id: 10,
-      name: 'Clementina DuBuque',
+      name: 'Anushri jain',
       username: 'Moriah.Stanton',
       email: 'Rey.Padberg@karina.biz'
     }
